73126d466c5087c581de3d246e7330059450be3c
Felipe Corrêa Ramos <lype086@gmail.com>
2016-07-16 14:37:23 -0300

    Instalation and configuration of django-compressor

--- concessionaire/settings.py
+++ concessionaire/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'compressor',
     'vehicles',
 ]
 
@@ -70,6 +71,12 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 
 WSGI_APPLICATION = 'concessionaire.wsgi.application'
 
@@ -124,3 +131,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.cssmin.rCSSMinFilter',
+)
+
+COMPRESS_JS_FILTERS = (
+    'compressor.filters.jsmin.JSMinFilter',
+)
+
+COMPRESS_OUTPUT_DIR = ''
